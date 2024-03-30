c40a933a0e40c1e436ccef12fa916b821a84777b
DESKTOP-BCBFLT7\Alex <alexvall24@gmail.com>
2024-03-30 17:44:26 +0100

    Se termina el ejercicio5

--- Ejercicio5/ejercicio5.java
+++ Ejercicio5/ejercicio5.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class ejercicio5 {
 
-    public static void main(String arg[]) {
+    public static void main(String[] arg) {
         String nombres[] = { "Víctor Robles", "Antonio Alcantara", "Al Pacino",
                 "Robert DeNiro", "Brad Pitt", "Sylvester Stallone" };
         ArrayList<String> nombresLista = new ArrayList<>(Arrays.asList(nombres));
         ArrayList<String> ordenarPorApellidos = new ArrayList<>();
+        ArrayList<String> nombresCompletosApellidosOrdenados = new ArrayList<>();
         for (int i = 0; i < nombresLista.size(); i++) {
-            ordenarPorApellidos.add(nombresLista.get(i).split(" "));
+            String nombresApellidos[] = nombresLista.get(i).split(" ");
+            ordenarPorApellidos.add(nombresApellidos[1]);
+            ordenarPorApellidos.sort(null);
         }
-
+        for (String apellido : ordenarPorApellidos) {
+            for (String nombreCompleto : nombresLista) {
+                if (nombreCompleto.contains(apellido)) {
+                    nombresCompletosApellidosOrdenados.add(nombreCompleto);
+                    System.out.println(nombreCompleto);
+                }
+            }
+        }
     }
 }
