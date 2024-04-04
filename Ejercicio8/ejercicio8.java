1b9e794b5a5c5b36b035a6714a34875ac34a0a37
AlexVallTuru <vall_06_@hotmail.com>
2024-04-04 15:46:25 +0200

    Solcionar problema

--- Ejercicio8/ejercicio8.java
+++ Ejercicio8/ejercicio8.java
@@ -9,7 +9,7 @@ public class ejercicio8 {
     public static void main(String[] arg) {
 
         String[] simbolosRomanos = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-        int[] valoresDecimales = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 5, 4, 1 };
+        int[] valoresDecimales = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
         Scanner in = new Scanner(System.in);
         System.out.println("Introduce un numero a que quieras transformar en numeros romanos:");
