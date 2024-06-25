81f8b5207943b154345ea1bc210b84f4ebf8d2aa
AlexVallTuru <vall_06_@hotmail.com>
2024-06-25 11:24:20 +0200

    Ejercicio17

--- Ejercicio17/ejercicio17.java
+++ Ejercicio17/ejercicio17.java
@@ -1,5 +1,20 @@
-package Ejercicio17;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ejercicio17 {
-    
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+
+        @SuppressWarnings("rawtypes")
+        ArrayList<String> parrafo = new ArrayList(Arrays.asList("Hola, soy Víctor Robles WEB","Me gusta programar","Y soy desarrollador web"));
+        ArrayList<String> palabras = new ArrayList<>();
+
+        for(int i = 0; i < parrafo.size(); i++){
+            palabras.addAll(Arrays.asList(parrafo.get(i).split(" ")));    
+        }
+
+        System.out.print(palabras.size());
+    }
+
 }
