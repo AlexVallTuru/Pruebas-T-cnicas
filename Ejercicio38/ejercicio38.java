d897ae236687efdd2547ab4f5d90cbf4f74f2ea8
AlexVallTuru <vall_06_@hotmail.com>
2024-07-09 10:11:37 +0200

    Ejercicio38

--- Ejercicio38/ejercicio38.java
+++ Ejercicio38/ejercicio38.java
@@ -1,3 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ejercicio38 {
     
+    final static int divisor = 2;
+
+    public static void main(String[] args) {
+        
+        aBinario(11342222222223);
+        
+    }
+
+    public static void aBinario(int dividendo){
+
+        int residuo;
+        List<Integer> numBinario = new ArrayList<>();
+
+        while (dividendo > 0) {
+            residuo = dividendo % divisor;
+            dividendo = dividendo / divisor;
+            numBinario.add(residuo);            
+        }
+        //String numBinarioFinal ="";
+        StringBuilder numBinarioFinal = new StringBuilder();
+        Collections.reverse(numBinario);
+        for(int num:numBinario){
+            //numBinarioFinal = numBinarioFinal.concat((String.valueOf(num)));
+            numBinarioFinal.append(num);
+        }
+        System.out.println(numBinarioFinal.toString());
+
+    }
+
 }
