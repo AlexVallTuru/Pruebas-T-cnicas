e8dece99f2118d489251053aa2d9dd1cfd9e1ae2
AlexVallTuru <vall_06_@hotmail.com>
2024-07-11 08:50:46 +0200

    Ejercicio41

--- Ejercicio41/ejercicio41.java
+++ Ejercicio41/ejercicio41.java
@@ -1,9 +1,32 @@
-package Ejercicio41;
 
 public class ejercicio41 {
-    
+
     public static void main(String[] args) {
-        
+
+        invertirNumeros(45);
+
+    }
+
+    public static void invertirNumeros(int numero) {
+        /*
+         * String numeroEditado = String.valueOf(numero);
+         * 
+         * if (numeroEditado.contains("-")) {
+         * numeroEditado = numeroEditado.replace("-", "");
+         * }else {
+         * numeroEditado = "-" + numeroEditado;
+         * }
+         * 
+         * numero = Integer.parseInt(numeroEditado);
+         * System.out.println(numero);
+         */
+
+        if (numero < 0) {
+            numero = -numero;
+        } else {
+            numero = +numero;
+        }
+        System.out.println(numero);
     }
 
 }
